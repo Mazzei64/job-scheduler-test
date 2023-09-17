0a5f0d57b089ee0e0a0b4dd4342314528be9d886
Mazzei64 <mazzei.ws@gmail.com>
2023-09-17 18:40:16 -0300

    feature: defines two unitary tests for JobScheduler module.

--- tests/JobScheduler.test.ts
+++ tests/JobScheduler.test.ts
@@ -1,42 +1,34 @@
-// JobScheduler.test.ts
-
-// import JobScheduler from './../JobScheduler';
+import JobScheduler from './../JobScheduler';
 import Job from './../interfaces/Job';
 import jobs from './SampleJobs';
 
 describe('JobScheduler', () => {
 
   it('should group jobs correctly', () => {
-    // // Create some sample jobs for testing
     
+    const result = JobScheduler(jobs);
 
-    // // Call the JobScheduler function
-    // const result = JobScheduler(jobs);
-
-    // // Write assertions to check if the function produces the expected output
-    // expect(result).toEqual([
-    //   [
-    //     { ID: 1, tempoEstimado: 100, dataMaxima: expect.any(Date) },
-    //     { ID: 2, tempoEstimado: 200, dataMaxima: expect.any(Date) },
-    //   ],
-    //   [
-    //     { ID: 3, tempoEstimado: 300, dataMaxima: expect.any(Date) },
-    //   ],
+    expect(result).toEqual([
+      [
+        { ID: 1, descricao: 'Salvar submarino submerso', tempoEstimado: 100, dataMaxima: expect.any(Date) },
+        { ID: 2, descricao: 'Salvar o mundo da fome', tempoEstimado: 200, dataMaxima: expect.any(Date) },
+      ],
+      [
+        { ID: 3, descricao: 'Lavar a louça de domingo', tempoEstimado: 300, dataMaxima: expect.any(Date) },
+      ],
       
-    // ]);
+    ]);
   });
 
   it('should throw an error if any job exceeds the time limit', () => {
     // Create a job that exceeds the time limit
-    // const jobs: Job[] = [
-    //   { ID: 1, tempoEstimado: 500, dataMaxima: new Date(), descricao: 'random descricao' },
-    // ];
+    const jobs: Job[] = [
+      { ID: 1, tempoEstimado: 500, dataMaxima: new Date(), descricao: 'random descricao' },
+    ];
 
-    // // Use Jest's expect().toThrow() to check if the function throws an error
-    // expect(() => {
-    //   JobScheduler(jobs);
-    // }).toThrowError(/Job 1 has its estimated execution time higher than the proposed limit/);
+    expect(() => {
+      JobScheduler(jobs);
+    }).toThrowError(/Job 1 has its estimated execution time higher than the proposed limit/);
   });
 
-  // Add more test cases as needed
 });
--- tests/SampleJobs.ts
+++ tests/SampleJobs.ts
@@ -1,8 +1,9 @@
 import Job from './../interfaces/Job';
 
 const jobs: Job[] = [
-    { ID: 1, tempoEstimado: 100, dataMaxima: new Date(), descricao: "Software Development" },
-    // ... other objects ...
+    { ID: 1, descricao: 'Salvar submarino submerso', tempoEstimado: 100, dataMaxima: new Date() },
+    { ID: 2, descricao: 'Salvar o mundo da fome', tempoEstimado: 200, dataMaxima: new Date() },
+    { ID: 3, descricao: 'Lavar a louça de domingo', tempoEstimado: 300, dataMaxima: new Date() },
   ];
   
   export default jobs;
