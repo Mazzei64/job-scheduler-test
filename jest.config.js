996f539b50acba30c7cc0d0e6fe813d226008b7c
Mazzei64 <mazzei.ws@gmail.com>
2023-09-17 19:51:19 -0300

    feature: adds jest.config file to the project and allows it in gitignore.

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+
+module.exports = {
+    testEnvironment: 'node', // Specify the test environment
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest', // Use ts-jest for TypeScript files
+    },
+  };
+  
